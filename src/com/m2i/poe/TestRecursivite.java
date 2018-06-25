eee428866a19339f55527a7ac631f03f739203dc
baptistehun <hun.baptiste@orange.fr>
2018-06-25 16:49:29 +0200

    Ajout multiplication

--- src/com/m2i/poe/TestRecursivite.java
+++ src/com/m2i/poe/TestRecursivite.java
@@ -2,7 +2,6 @@ package com.m2i.poe;
 
 public class TestRecursivite {
 
-
     public static int factorielle(int n){
         if (n==0 || n == 1){
             return 1;
@@ -10,8 +9,18 @@ public class TestRecursivite {
         return n * factorielle(n-1);
     }
 
-    public static void main (String[] args){
-        System.out.println(factorielle(6));
+    public static int multiplication(int[] tab,int n){
+        if (n == 1){
+            return tab[0];
+        }
+        else {
+            return tab[n-1] * multiplication(tab, n-1);
+        }
 
+
+    }
+    public static void main (String[] args){
+        int[] tab = {2,3,4,5,6};
+        System.out.println(multiplication(tab, tab.length));
     }
 }
