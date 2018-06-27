21b1780eb7ebbcfb0a805b40dcdd9772e32fa9d1
baptistehun <hun.baptiste@orange.fr>
2018-06-27 14:44:16 +0200

    Passage private

--- src/com/m21/poe/bank/Account.java
+++ src/com/m21/poe/bank/Account.java
@@ -4,24 +4,32 @@ import java.util.Date;
 
 public class Account {
     //    Attributs
-    String id;
-    double solde = 0;
-    boolean isActive = true;
-    boolean isBlocked = false;
-    Date creationDate = new Date();
+    private int id  = 0;
+    private double solde = 0;
+    private boolean isActive = true;
+    private boolean isBlocked = false;
+    private Date creationDate = new Date();
 
 //    Méthodes
 
+    public Account(int id){
+        this.id = id;
+    }
+
+    public Account(){
+
+    }
+
     void deposit(double amount) {
-        if (isActive && !isBlocked) {
-            solde += amount;
+        if (isActive() && !isBlocked()) {
+            solde = (getSolde() + amount);
         }
     }
 
     double withdraw(double amount) {
-        if (isActive && !isBlocked) {
-            if ((amount <= solde)) {
-                solde -= amount;
+        if (isActive() && !isBlocked()) {
+            if ((amount <= getSolde())) {
+                solde = (getSolde() - amount);
                 return amount;
             } else {
                 return 0;
@@ -42,5 +50,30 @@ public class Account {
 
     void unblock() {
         isBlocked = false;
+    }
+
+//    GETTER & SETTER
+    public int getId() {
+        return id;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public boolean isBlocked() {
+        return isBlocked;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public String toString(){
+        return "le solde est : " + getSolde() + " l\'id est : " + getId();
     }
 }
--- src/com/m21/poe/bank/Bank.java
+++ src/com/m21/poe/bank/Bank.java
@@ -4,23 +4,20 @@ public class Bank {
 
     public static void main(String[] args){
         Account a1 = new Account();
-        a1.solde = 5000;
-        a1.deposit(100);
-        System.out.println(a1.solde);
+
+        System.out.println(a1);
         a1.withdraw(50);
-        System.out.println(a1.solde);
         double d = a1.withdraw(1000);
-        System.out.println(a1.solde);
         a1.block();
         a1.deposit(100);
-        System.out.println(a1.solde);
+        System.out.println(a1);
         a1.unblock();
         a1.deposit(100);
-        System.out.println(a1.solde);
+        System.out.println(a1);
         a1.close();
-        System.out.println(a1.solde);
+        System.out.println(a1);
         a1.withdraw(300);
-        System.out.println(a1.solde);
+        System.out.println(a1);
 
 
 
