b17723aacf3370bc2e4ec677541d1e18ccdace4f
Baptiste <baptiste.hun@laposte.net>
2018-09-03 22:27:20 +0200

    maj

--- POEJava/src/main/java/com/m2i/poe/media/TestDBConnection.java
+++ POEJava/src/main/java/com/m2i/poe/media/TestDBConnection.java
@@ -2,11 +2,13 @@ package com.m2i.poe.media;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import com.microsoft.sqlserver.jdbc.SQLServerDriver;
+
 
 public class TestDBConnection {
 
     public static void main(String[] args) {
-        String Url = "jdbc:sqlserver://FORMATI-PBT3D4L\\SQLEXPRESS;DatabaseName=formation;user=sa;password=cgecge";
+        String Url = "jdbc:sqlserver://DESKTOP-A0EMVTR\\SQLEXPRESS;DatabaseName=formation;user=sa;password=cgecge";
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
---
+++ POEJava/src/main/java/com/m2i/poe/rest/GeneticRest.java
@@ -0,0 +1,14 @@
+package com.m2i.poe.rest;
+
+
+import com.m2i.poe.media.Book;
+import com.m2i.poe.media.BookJPARepository;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.util.List;
+
+@Path("/Genetic")
+public class GeneticRest {
+
+
+}
